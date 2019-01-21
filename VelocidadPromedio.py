d4dbeef13230ca0775e07c18e4e68a1b20cb7611
JuanCarlos-A01376511 <42353013+JuanCarlos-A01376511@users.noreply.github.com>
2019-01-21 09:40:22 -0600

    Juan Carlos Flores García, Grupo 02

---
+++ VelocidadPromedio.py
@@ -0,0 +1,11 @@
+# Autor: Juan Carlos Flores García A01376511.
+
+# Calcula la velocidad promedio de un viaje.
+
+tiempo = int(input("Teclea el tiempo del viaje en horas enteras: "))
+distancia = int(input("Teclea la distancia del viaje en kilómetros enteros: "))
+
+velocidad = distancia / tiempo
+
+print("La velocidad promedio es: ", velocidad, "km/h")
+
